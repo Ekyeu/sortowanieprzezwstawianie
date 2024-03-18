f9a188b99d1fdcce90634855c9d00ab2d5177fa6
Ekyeu <115065962+Ekyeu@users.noreply.github.com>
2024-03-18 11:41:20 +0100

    Add files via upload

---
+++ sortowanieprzezwstawianie/sortowanieprzezwstawianie/Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random r = new Random();
+        int[] tab1 = new int[100];
+        int[] tab2 = tab1;
+
+        // uzupelnienie i wyswietlenie
+
+        Console.WriteLine("Tablica przed sortowaniem");
+        for (int i = 0; i < 100; i++)
+        {
+            tab1[i] = r.Next(1, 100);
+            Console.Write(tab1[i] + "\n");
+        }
+
+        
+        // sortowanie
+
+        InsertionSort(tab1);
+
+        Console.WriteLine("\n Tablica Posortowana");
+        for (int i = 0; i < tab1.Length; i++)
+        {
+            Console.Write(tab1[i] + "\n");
+        }
+
+
+
+        // sprawdzenie
+
+        Array.Sort(tab2);
+        Console.WriteLine("\n Sprawdzenie czy tablica zostala dobrze posortowana");
+
+        for (int i = 0; i < tab2.Length; i++)
+        {
+            Console.Write(tab2[i] + "\n");
+        }
+
+    }
+    static void InsertionSort(int[] list)
+    {
+        int i = 1;
+        while (i < list.Length)
+        {
+            // reset parametrow
+            int x = list[i];
+            int j = i;
+
+            while (j > 0 && list[j - 1] > x)
+            {
+                list[j] = list[j - 1];
+                j = j - 1;
+            }
+            list[j] = x;
+            i = i + 1;
+        }
+    }
+}
+
